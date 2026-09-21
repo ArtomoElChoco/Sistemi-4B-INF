5fa8c232528ecb64da8a900125da38ac6fb0324e
unknown <a.cojocaru.3950@vallauri.net>
2026-09-21 09:40:24 +0200

    02_aritmetica fatto

--- Puntatori/02_aritmetica.c
+++ Puntatori/02_aritmetica.c
@@ -4,10 +4,22 @@ int main(void){
     
     int v[] = {33, 44, 55};
     int i;
+    int *pV;
 
-    for(i = 0; i < 4; i++){
-        printf("v[%d]: %d\n", i, v[i]);
+    for(i = 0; i < 3; i++){
+        // printf("v[%d]: %d indirizzo: %p\n", i, v[i], &v[i]);
+        printf("v[%d]: %d indirizzo: %p\n", i, *(v + i), (v+i));
     }
+    
+    // *p = 
+    // p = 
+    // &p = 
+    
+    pV = v;
+
+    printf("Prima cella: %d con indirizzo %p\n", *pV, pV);
+    pV++;
+    printf("Seconda cella: %d con indirizzo %p\n", *pV, pV);
 
     return 0;
 }
